9c9572e3ccb8fd8b5ea23f68553cb1c102da6750
Frank Cedeño <frank.c.rojas@gmail.com>
2020-10-11 15:22:56 +0200

    Magento 2 Module Development - Simple Hello World Module

---
+++ app/code/BlessedCode/HelloWorld/Controller/Index/Test.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace BlessedCode\HelloWorld\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Test extends Action
+{
+    protected $_pageFactory;
+
+    public function __construct(
+        Context $context,
+        PageFactory $pageFactory)
+    {
+        $this->_pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        echo "Hello World";
+        exit;
+    }
+}
